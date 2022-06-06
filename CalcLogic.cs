f7ec7c2019a37c51a603495ef8ea34265deb643c
bazdu <bazdukovvalentin@gmail.com>
2022-06-06 10:17:19 +1000

    Add second class. Add digital buttons logic

---
+++ CalcLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Calculator;
+
+public class CalcLogic
+{
+    public string inputBuffer = "";
+    public string outputBuffer = "";
+    
+    public void SendToInputBuffer(string elem)
+    {
+        inputBuffer += elem;
+        
+    }
+
+    
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,43 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        internal CalcLogic calc = new CalcLogic();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void Digital_Button(object sender, EventArgs e)
+        {
+            if (inputBuffer.Text.Length < 9)
+            {
+                calc.SendToInputBuffer((sender as Button).Content.ToString());
+                inputBuffer.Text = calc.inputBuffer;
+            }
+        }
+
+        public void DoFloatingNum(object sender, EventArgs e)
+        {
+            if (!calc.inputBuffer.Contains(",") && calc.inputBuffer != String.Empty)
+            {
+                calc.SendToInputBuffer((sender as Button).Content.ToString());
+                inputBuffer.Text = calc.inputBuffer;
+            }
+        }
+
+        public void DeleteLastNum(object sender, EventArgs e)
+        {
+            if (calc.inputBuffer != string.Empty || calc.inputBuffer != "0")
+            {
+                calc.inputBuffer = calc.inputBuffer.Remove(calc.inputBuffer.Length - 1);
+                inputBuffer.Text = calc.inputBuffer;
+                if (inputBuffer.Text.Length == 1)
+                {
+                    inputBuffer.Text = "0";
+                }
+            }
+
+            
         }
     }
 }
